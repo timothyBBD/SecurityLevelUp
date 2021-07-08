2c754638f3a3c49797f7cee8bc86553b269f55cd
Gian Uys <gianuys@discovery.bank>
2021-07-09 00:01:40 +0200

    Added Home page with articles and about preview in responsive container.

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,26 +1,72 @@
 import logo from './logo.svg';
 import './App.css';
 import Components from './components/index'
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
+
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+
+import Button from 'react-bootstrap/Button'
+
+import Media from 'react-media';
+
+import TheArticle from './pages/Articles/Articles';
 
 const Article = Components.Article;
+const AboutPreview = Components.AboutPreview;
+
 function App() {
+
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-      <Article title="Cleaning out emotional Clutter" message="Lorem Ipsum or something like that"/>
+
+      {/* <BlogNavBar/> */}
+
+      <Router>
+        <nav>
+            <ul>
+              <li>
+                <Link to="/">Home</Link>
+              </li>
+              <li>
+                <Link to="/articles">Articles</Link>
+              </li>
+              <li>
+                <Link to="/users">Users</Link>
+              </li>
+            </ul>
+          </nav>
+
+        <Switch>
+          <Route path="/articles">
+            <Button variant="primary" size="lg">
+              Large button
+            </Button>
+
+            <Container>
+              <Row>
+                <Col className='testytest' md={{order: "first", span: 8}} xs={{order: "last", span: 12}}>
+                  <Article title="Cleaning out emotional Clutter" message="Lorem Ipsum or something like that"/>
+                </Col>
+                <Col className='testytest' md={{order: "last", span: 4}} xs={{order: "first", span: 12}}>
+                  <AboutPreview/>
+                </Col>
+              </Row>
+            </Container>
+
+          </Route>
+
+        </Switch>
+      </Router>
+
     </div>
   );
 }
---
+++ frontend/src/components/aboutPreview/aboutPreview.js
@@ -0,0 +1,9 @@
+import React, { useState } from 'react';
+import './aboutPreview.css';
+
+export default function AboutPreview() {
+    return (
+        <p>this is your about preview</p>
+
+    )
+}
