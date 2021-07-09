c58270c55ecdb0c579e2e63ff778007505224718
Gian Uys <gianuys@discovery.bank>
2021-07-09 22:03:36 +0200

    Integrated article and loginModel components.

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,13 +1,8 @@
 import React, { useState } from 'react';
 import logo from './logo.svg';
 import './App.scss';
-import Components from './components/index'
-import {
-  BrowserRouter as Router,
-  Switch,
-  Route,
-  Link
-} from "react-router-dom";
+import Components from './components/index';
+import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
 
 import Container from 'react-bootstrap/Container';
 import Row from 'react-bootstrap/Row';
@@ -17,22 +12,15 @@ import Nav from 'react-bootstrap/Nav';
 import NavDropdown from 'react-bootstrap/NavDropdown';
 import Modal from 'react-bootstrap/Modal';
 
-import Button from 'react-bootstrap/Button'
-
-import Media from 'react-media';
-
-import TheArticle from './pages/Articles/Articles';
-
+import Button from 'react-bootstrap/Button';
 const Article = Components.Article;
 const AboutPreview = Components.AboutPreview;
 
 const LoginModal = Components.LoginModal;
 
 function App() {
-
   const [show, setShow] = useState(false);
   const [page, setPage] = useState(0);
-
 
   const handleClose = () => setShow(false);
   const handleShow = () => setShow(true);
@@ -42,76 +30,105 @@ function App() {
       border: '1px solid black',
       borderRadius: '8px',
       backgroundColor: 'black',
-      color: 'white'
+      color: 'white',
     },
-    false: {
-    }
-  }
+    false: {},
+  };
 
   return (
-    <div className="App">
-
-      {/* <BlogNavBar/> */}
-
-      <Modal show={show} onHide={handleClose}>
+    <div className='App'>
+      <Modal show={show} onHide={handleClose} size='lg' >
         <Modal.Body>
-          <LoginModal/>
+          <LoginModal />
           
-          </Modal.Body>
-        <Modal.Footer>
-          <Button variant="secondary" onClick={handleClose}>
+        </Modal.Body>
+
+
+        {/* <Modal.Footer>
+          <Button variant='secondary' onClick={handleClose}>
             Close
           </Button>
-          <Button variant="primary" onClick={handleClose}>
+          <Button variant='primary' onClick={handleClose}>
             Save Changes
           </Button>
-        </Modal.Footer>
+        </Modal.Footer> */}
+
+
       </Modal>
 
       <Router>
+        <Navbar collapseOnSelect expand='lg' className='py-4' variant='light'>
+          <Container>
+            <Navbar.Brand to='/' className='brandName' >Simplicity Blog</Navbar.Brand>
+            <Navbar.Toggle aria-controls='responsive-navbar-nav' />
+            <Navbar.Collapse id='responsive-navbar-nav'>
+              <Nav defaultActiveKey='/articles' className='ml-auto'>
+                <Link
+                  style={pageButtonStyle[page === 0]}
+                  className='pageButtonStyle mx-3'
+                  onClick={() => {
+                    setPage(0);
+                  }}
+                  to='/articles'
+                >
+                  Articles
+                </Link>
 
-      <Navbar collapseOnSelect expand="lg" className="py-4" variant="light">
-        <Container>
-          <Navbar.Brand href="#home">Simplicity Blog</Navbar.Brand>
-          <Navbar.Toggle aria-controls="responsive-navbar-nav"  />
-          <Navbar.Collapse id="responsive-navbar-nav">
+                <Link
+                  style={pageButtonStyle[page === 1]}
+                  className='pageButtonStyle mx-3'
+                  onClick={() => {
+                    setPage(1);
+                  }}
+                  to='/about'
+                >
+                  About
+                </Link>
 
-            <Nav defaultActiveKey="/articles" className="ml-auto" >
+                <Link
+                  style={pageButtonStyle[page === 2]}
+                  className='pageButtonStyle mx-3'
+                  onClick={() => {
+                    setPage(2);
+                    handleShow();
+                  }}
+                >
+                  Register/Login
+                </Link>
+              </Nav>
+            </Navbar.Collapse>
+          </Container>
+        </Navbar>
 
-              <Link style={pageButtonStyle[page === 0]} className="pageButtonStyle mx-3" onClick={() => {setPage(0)}} to="/articles">Articles</Link>
-
-              <Link style={pageButtonStyle[page === 1]} className="pageButtonStyle mx-3" onClick={() => {setPage(1)}} to="/about">About</Link>
-
-              <Link style={pageButtonStyle[page === 2]} className="pageButtonStyle mx-3" onClick={() => {setPage(2); handleShow();}} >Register/Login</Link>
-
-            </Nav>
-
-          </Navbar.Collapse>
-        </Container>
-      </Navbar>
-
-  
-        <Switch>
-          <Route path="/articles">
-            <Container>
-              <Row>
-                <Col className='testytest' md={{order: "first", span: 8}} xs={{order: "last", span: 12}}>
-                  <Article title="Cleaning out emotional Clutter" message="Lorem Ipsum or something like that"/>
-                </Col>
-                <Col className='testytest' md={{order: "last", span: 4}} xs={{order: "first", span: 12}}>
-                  <AboutPreview/>
-                </Col>
-              </Row>
-            </Container>
-
-          </Route>
-          <Route path="/about">
-            <AboutPreview/>
-          </Route>
-
-        </Switch>
+        <div className='mainContent'>
+          <Switch>
+            <Route path='/articles'>
+              <Container>
+                <Row >
+                  <Col className='testytest'
+                    md={{ order: 'first', span: 8 }}
+                    xs={{ order: 'last', span: 12 }}
+                  >
+                    <Article
+                      title='Cleaning out emotional Clutter'
+                      message='Lorem Ipsum or something like that'
+                    />
+                  </Col>
+                  <Col className='testytest'
+                    md={{ order: 'last', span: 4 }}
+                    xs={{ order: 'first', span: 12 }}
+                  >
+                    <AboutPreview />
+                  </Col>
+                </Row>
+              </Container>
+            </Route>
+            <Route path='/about'>
+              <AboutPreview />
+            </Route>
+          </Switch>
+        </div>
       </Router>
-
     </div>
   );
 }
