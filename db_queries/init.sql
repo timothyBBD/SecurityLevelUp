f8703879f8e15a4ea8cfe17f5e83235125ffc4e8
Timothy <Timothy@bbd.co.za>
2021-07-13 09:53:39 +0200

    fixed mysql init file

--- db_queries/init.sql
+++ db_queries/init.sql
@@ -2,6 +2,8 @@ DROP DATABASE IF EXISTS simplicity_db;
 CREATE DATABASE simplicity_db;
 USE simplicity_db;
 
+
+
 CREATE TABLE users (
 	id BINARY(16) PRIMARY KEY,
     user_name VARCHAR(50) CHARACTER SET utf8mb4 UNIQUE,
@@ -37,22 +39,14 @@ CREATE TABLE posts (
 	FOREIGN KEY (user_id) REFERENCES users(id)
 );
 
-CREATE 
-    ALGORITHM = UNDEFINED 
-    DEFINER = `root`@`localhost` 
-    SQL SECURITY DEFINER
-VIEW `simplicity_db`.`blog_posts_view` AS
+CREATE VIEW `simplicity_db`.`blog_posts_view` AS
     SELECT 
         `simplicity_db`.`posts`.`post_title` AS `title`,
         `simplicity_db`.`posts`.`post_body` AS `body`
     FROM
         `simplicity_db`.`posts`;
 
-CREATE 
-    ALGORITHM = UNDEFINED 
-    DEFINER = `root`@`localhost` 
-    SQL SECURITY DEFINER
-VIEW `simplicity_db`.`password_salt_view` AS
+CREATE VIEW `simplicity_db`.`password_salt_view` AS
     SELECT 
         CAST(HEX(`simplicity_db`.`salts`.`salt_value`)
             AS CHAR (100) CHARSET UTF8MB4) AS `encryptedSalt`,
@@ -64,31 +58,35 @@ VIEW `simplicity_db`.`password_salt_view` AS
         JOIN `simplicity_db`.`passwords` ON ((`simplicity_db`.`users`.`id` = `simplicity_db`.`passwords`.`user_id`)))
         JOIN `simplicity_db`.`salts` ON ((`simplicity_db`.`passwords`.`id` = `simplicity_db`.`salts`.`password_id`)));
 
-CREATE DEFINER=`root`@`localhost` PROCEDURE `sp_add_blog_post`(IN title VARCHAR(250), IN body TEXT, IN userId BINARY(16) )
-BEGIN
+
+CREATE PROCEDURE `sp_add_blog_post`(IN title VARCHAR(250), IN body TEXT, IN userId BINARY(16) )
 	INSERT INTO posts(id, post_title, post_body, user_id)
     VALUES(UUID_TO_BIN(UUID()), title, body, userId);
-END
 
-CREATE DEFINER=`root`@`localhost` PROCEDURE `sp_add_user_and_password`(IN userName VARCHAR(50), IN isAdminUser BOOL, IN passwordHash VARCHAR(100), IN passwordSalt VARCHAR(100), IN emailAddress VARCHAR(320))
+DELIMITER $$   
+CREATE DEFINER=`root`@`localhost` PROCEDURE `sp_user_details`(IN userName VARCHAR(50))
 BEGIN
+	SELECT users.id AS userId, admin_user, email
+	FROM users
+	INNER JOIN emails ON users.id = emails.user_id
+	WHERE users.user_name = userName;
+END$$
 
-	DECLARE passwordId BINARY(16); 
+
+    
+
+DELIMITER $$
+CREATE PROCEDURE `sp_add_user_and_password`(IN userName VARCHAR(50), IN isAdminUser BOOL, IN passwordHash VARCHAR(100), IN passwordSalt VARCHAR(100), IN emailAddress VARCHAR(320))
+BEGIN
+	DECLARE passwordId BINARY(16);
     DECLARE binary_password BINARY(32);
 	DECLARE binary_salt  VARBINARY(100);
     DECLARE user_id BINARY(16);
     
 	DECLARE EXIT HANDLER FOR SQLEXCEPTION
-	BEGIN
 		ROLLBACK;
 		SELECT 'Duplicate key occurred' AS message;
-    END;
-	DECLARE EXIT HANDLER FOR SQLWARNING
-	BEGIN
-		ROLLBACK;
-		SELECT 'Duplicate key occurred' AS message;
-    END;
-
+        
 	START TRANSACTION;
         SET user_id = UUID_TO_BIN(UUID());
 		SET binary_password = UNHEX(passwordHash);
@@ -107,37 +105,37 @@ BEGIN
 		INSERT INTO salts(id, salt_value, password_id)
 		VALUES (UUID_TO_BIN(UUID()), binary_salt, passwordId);
 	COMMIT;
-END
-
+END$$
+    
+    
 CREATE DEFINER=`root`@`localhost` PROCEDURE `sp_password_hash_salt`(IN userId BINARY(16))
 BEGIN
 	SELECT passwordHash, encryptedSalt FROM simplicity_db.password_salt_view
     WHERE user_id = userId;
-END
+END$$
 
-CREATE DEFINER=`root`@`localhost` PROCEDURE `sp_user_details`(IN userName VARCHAR(50))
-BEGIN
-	SELECT users.id AS userId, admin_user, email
-	FROM users
-	INNER JOIN emails ON users.id = emails.user_id
-	WHERE users.user_name = userName;
-END
 
-CREATE USER [IF NOT EXISTS] 'simplicity_server'@'localhost'
-IDENTIFIED BY '6%SYsRmymokmjG5!rzCS6ZcemGSs6@'
+DROP USER IF EXISTS 'simplicity_server'@'localhost';
+CREATE USER 'simplicity_server'@'localhost'
+IDENTIFIED BY '6%SYsRmymokmjG5!rzCS6ZcemGSs6@';
+GRANT INSERT ON simplicity_db.users TO 'simplicity_server'@'localhost';
+GRANT INSERT ON simplicity_db.emails TO 'simplicity_server'@'localhost';
+GRANT INSERT ON simplicity_db.posts TO 'simplicity_server'@'localhost';
+GRANT INSERT ON simplicity_db.passwords TO 'simplicity_server'@'localhost';
+GRANT INSERT ON simplicity_db.salts TO 'simplicity_server'@'localhost';
+GRANT UPDATE ON simplicity_db.users TO 'simplicity_server'@'localhost';
+GRANT UPDATE ON simplicity_db.emails TO 'simplicity_server'@'localhost';
+GRANT UPDATE ON simplicity_db.posts TO 'simplicity_server'@'localhost';
+GRANT UPDATE ON simplicity_db.passwords TO 'simplicity_server'@'localhost';
+GRANT UPDATE ON simplicity_db.salts TO 'simplicity_server'@'localhost';
+GRANT SELECT ON simplicity_db.users TO 'simplicity_server'@'localhost';
+GRANT SELECT ON simplicity_db.emails TO 'simplicity_server'@'localhost';
+GRANT SELECT ON simplicity_db.posts TO 'simplicity_server'@'localhost';
+GRANT SELECT ON simplicity_db.passwords TO 'simplicity_server'@'localhost';
+GRANT SELECT ON simplicity_db.salts TO 'simplicity_server'@'localhost';
+GRANT EXECUTE ON `simplicity_db`.* TO 'simplicity_server'@'localhost';
+-- GRANT EXECUTE ON simplicity_db.sp_add_user_and_password TO 'simplicity_server'@'localhost';
+-- GRANT EXECUTE ON simplicity_db.sp_password_hash_salt TO 'simplicity_server'@'localhost';
+-- GRANT EXECUTE ON simplicity_db.sp_user_details TO 'simplicity_server'@'localhost';
 
-GRANT permission_type
-ON INSERT 
-TO 'simplicity_db'@'localhost';
 
-GRANT permission_type
-ON SELECT 
-TO 'simplicity_db'@'localhost';
-
-GRANT permission_type
-ON UPDATE 
-TO 'simplicity_db'@'localhost';
-
-GRANT permission_type
-ON EXECUTE
-TO 'simplicity_db'@'localhost';
