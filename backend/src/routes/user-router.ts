1d07e94c4de387a601fbdf9448ae067ce7b0d6a7
MarnusJB <84138147+MarnusJB@users.noreply.github.com>
2021-07-13 08:52:39 +0200

    fix

--- backend/src/routes/user-router.ts
+++ backend/src/routes/user-router.ts
@@ -7,7 +7,7 @@ import { body, validationResult } from 'express-validator';
 const userRouter = express.Router();
 
 userRouter.route('/').post(body("email").isEmail(), body("password").isLength({ min: 8 }), body("username").not().isEmpty().trim().escape(), userController.registerUser)
-userRouter.route('/login').post(body("email").isEmail(), body("password").isLength({ min: 8 }), userController.loginUser)
+userRouter.route('/login').post(body("username").not().isEmpty().trim().escape(), body("password").isLength({ min: 8 }), userController.loginUser)
 userRouter.route('/forgot-password').post(userController.forgotPassword)
 
 export default userRouter;
